e65fb3b6b896593d97cae1df8d8681705501f077
ritik saini <ritik23saini@gmail.com>
2025-06-18 13:43:20 +0530

    photo

--- cloneproject/src/App.jsx
+++ cloneproject/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
 
 
-    <div className=" min-h-screen flex flex-col  " >
+    <div className=" min-h-screen flex flex-col  justify-center items-center " >
       <Nav />
       <div className=" flex justify-center  m-5">
         <div className="flex flex-col text-center justify-center  ">
@@ -18,12 +18,12 @@ function App() {
           <h2>FOR SCHOOL/COLLEGE STUDENTS & WORKING PROFESSIONALS</h2>
         </div>
         <div className="flex align-center justify-center">
-          <img src="/src/assets/image.jpg" alt="Description of image" />
+          <img src="/src/assets/image.png" alt="Description of image" />
         </div>
       </div>
 
 
-      <div className="grid lg:grid sm:flex sm:flex-wrap lg:grid-cols-3 gap-3 justify-evenly">
+      <div className="grid lg:grid sm:flex sm:flex-wrap m-10 lg:grid-cols-3 gap-3 justify-evenly">
         <CarrerReportCard />
         <RecomendedPlanCard />
         <PopularplanCard />
--- cloneproject/src/components/Nav.jsx
+++ cloneproject/src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
     return (
-        <div className='fixed w-full bg-slate-200 shadow-xl flex justify-around items-center gap-2'>
+        <div className='w-full shadow-lg flex justify-around items-center gap-2'>
             <h2 className='text-2xl font-bold'>CareerGuide.com</h2>
 
             <div className='flex justify-evenly gap-3'> <Link> Class 10 </Link>
@@ -13,13 +13,13 @@ const Nav = () => {
                 <Link> Professionals </Link>
                 <Link> Study Aborad </Link>
                 <Link> Psychometric test </Link>
-                <Link> Pricing </Link>
+                <Link > Pricing </Link>
                 <Link> Courses </Link>
                 <p>918800442358</p>
             </div>
             <div className=' text-lg font-bold flex justify-center gap-3'>
-                <Link> login</Link>
-                <Link> signin</Link>
+                <Link> Signup</Link>
+                <Link> Login</Link>
 
             </div>
 
