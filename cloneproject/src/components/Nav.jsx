bac01408f6e1ddbfba320bfb5ca1d53ece7256fe
ritik saini <ritik23saini@gmail.com>
2025-06-18 13:55:53 +0530

    nav2

--- cloneproject/src/components/Nav.jsx
+++ cloneproject/src/components/Nav.jsx
@@ -3,26 +3,25 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
     return (
-        <div className='w-full h-[50px] shadow-lg flex justify-around items-center gap-2'>
+        <div className='w-full h-[50px] shadow-lg flex justify-around items-center gap-2 sticky top-0 z-50 bg-white'>
             <h2 className='text-2xl font-bold'>CareerGuide.com</h2>
 
-            <div className='flex justify-evenly gap-3'> <Link> Class 10 </Link>
-                <Link> Class 11-12 </Link>
-
-                <Link>Counsellor </Link>
-                <Link> Professionals </Link>
-                <Link> Study Aborad </Link>
-                <Link> Psychometric test </Link>
-                <Link > Pricing </Link>
-                <Link> Courses </Link>
+            <div className='flex justify-evenly gap-3'>
+                <Link>Class 10</Link>
+                <Link>Class 11-12</Link>
+                <Link>Counsellor</Link>
+                <Link>Professionals</Link>
+                <Link>Study Abroad</Link>
+                <Link>Psychometric test</Link>
+                <Link>Pricing</Link>
+                <Link>Courses</Link>
                 <p>918800442358</p>
             </div>
-            <div className=' text-lg font-bold flex justify-center gap-3'>
-                <Link> Signup</Link>
-                <Link> Login</Link>
 
+            <div className='text-lg font-bold flex justify-center gap-3'>
+                <Link>Signup</Link>
+                <Link>Login</Link>
             </div>
-
         </div>
     )
 }
